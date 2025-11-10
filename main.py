5fdf08f39217573c5a80ad8aea388c472f5796dc
Kevin Patel <kevinpa051@gmail.com>
2025-11-10 14:48:05 -0600

    Fix: clean Yahoo Finance multi-index + correct indicators

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ ticker = '^GSPC'
 
 #features = date, close, high, low, open, and volume
 data = yf.download(ticker, start='2024-01-01', end='2025-01-01')
+#get rid of multiIndex
+data.columns = data.columns.droplevel(1)
 
 #Calculation for Daily Return
 #add new feature to dataset - use Return predict future movements
@@ -19,7 +21,7 @@ data['SMA_50'] = data['Close'].rolling(50).mean()
 #momentum indicator
 #below is calculation of rsi
 delta = data['Close'].diff()
-gain = (delta.where(delta>0.0)).rolling(14).mean()
+gain = (delta.where(delta>0,0)).rolling(14).mean()
 loss = (-delta.where(delta<0,0)).rolling(14).mean()
 RS = gain/loss
 data['RSI'] = 100 - (100/(1+RS))
@@ -29,3 +31,9 @@ data['Volatility'] = data['Return'].rolling(10).std()
 #clean data
 data = data.dropna()
 
+#set up the target variable
+data['Target'] = data['Close'].shift(-1)
+data = data.dropna()
+
+
+print(data.head())
